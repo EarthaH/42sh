d4297edf7f2499f9947a3d9c9f4707f651dd8a87
Cameron DE BRUYN <cdebruyn@e5r7p1.jnb.42.fr>
2016-09-04 14:24:12 +0200

    split_commands

--- src/split_commands.c
+++ src/split_commands.c
@@ -1,8 +1,8 @@
 #include "../includes/minishell.h"
 
-void		split_commands(char *line)
+void			split_commands(char *line)
 {
-	char	**commands;
+	char			**commands;
 	unsigned int	i;
 
 	i = 0;
@@ -14,10 +14,24 @@ void		split_commands(char *line)
 	}	
 }
 
-void		validate_commands(char	**commands)
+void			validate_commands(char	**commands)
 {
 	if (created_commands(commands[0]) == 0)
-	if (defined_commands(commands[0]) == 0)
+//	if (defined_commands(commands[0]) == 0)
 	else
 		ft_putstr(M_MESS02);
+}
+
+static int		created_commands(char *command)
+{
+	if (ft_strcmp(command, "echo") == 0)
+		;
+	if (ft_strcmp(command, "cd") == 0)
+		;
+	if (ft_strcmp(command, "env") == 0)
+		;
+	if (ft_strcmp(command, "setenv") == 0)
+		;
+	if (ft_strcmp(command, "unsetenv") == 0)
+		;
 }
