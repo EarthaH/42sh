4833adceb65106ff1c05c61b129fa006bc1eca22
Cameron DE BRUYN <cdebruyn@e5r7p1.jnb.42.fr>
2016-09-04 12:42:16 +0200

    get_next_line.c

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cdebruyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/16 08:48:16 by cdebruyn          #+#    #+#             */
-/*   Updated: 2016/08/02 09:07:00 by cdebruyn         ###   ########.fr       */
+/*   Updated: 2016/09/04 12:40:13 by cdebruyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,5 +108,7 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+int				get_next_line(int const fd, char **line);
 
 #endif
